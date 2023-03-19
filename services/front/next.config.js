6aa3cec93fe66adcaeacd2740f1072381850a1da
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-03-19 10:52:21 +0100

    minor scripting updates

--- services/front/next.config.js
+++ services/front/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
