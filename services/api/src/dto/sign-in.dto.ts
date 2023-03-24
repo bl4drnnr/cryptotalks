5e5e889ded876bc3a8e7c51f333815660cc4e56f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-03-24 10:29:54 +0100

    shared dtos and events

--- services/api/src/dto/sign-in.dto.ts
+++ services/api/src/dto/sign-in.dto.ts
@@ -1,13 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
-
 export class SignInDto {
-  @IsEmail()
-  @IsString()
-  @IsNotEmpty()
   email: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @Length(6, 32)
   password: string;
 }
